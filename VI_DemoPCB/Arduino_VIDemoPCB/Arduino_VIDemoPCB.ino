4c689a6308a6c31fbf2808d201f014c02bcd1c5d
sideridispas <sideridispas@gmail.com>
2016-01-22 17:53:10 +0100

    22/01/2016_EndOfDay: Added code for 3 measurements (V1, V2, I) and also updated the processing file for that

--- VI_DemoPCB/Arduino_VIDemoPCB/Arduino_VIDemoPCB.ino
+++ VI_DemoPCB/Arduino_VIDemoPCB/Arduino_VIDemoPCB.ino
@@ -3,6 +3,7 @@
 long data, m_data;
 float f_data;
 ads12xx ads1256(7,2); //CS:7, DRDY:2
+int visual = 1; //processing app:1 - arduino serial monitor:0
 
 void reg_init(){
   unsigned long reg;
@@ -14,14 +15,11 @@ void reg_init(){
   reg = reg & B11110001; //ORDER, ACAL:0
   ads1256.SetRegisterValue(STATUS,reg);
   
-  //MUX register
-  ads1256.SetRegisterValue(MUX,B01000101); //AIN4 - AIN5
-  
   //ADCON register (default: 32)
-  ads1256.SetRegisterValue(ADCON,B00100110); //PGA:64
+  ads1256.SetRegisterValue(ADCON,B00100111); //PGA:64
   
   //DRATE register
-  ads1256.SetRegisterValue(DRATE,B00000011);
+  ads1256.SetRegisterValue(DRATE,B01100011); //01100011
  
   //Registers' printing
 /*  for(int i=0;i<5;i++){
@@ -35,14 +33,22 @@ void setup() {
   Serial.begin(9600);
   ads1256.begin();
   reg_init();  //register initialisation
+  delay(100);
+  ads1256.SendCMD(SELFCAL); //self-calibration command
+  delay(100);
 }
 
 void loop() {
- 
+
+  f_data = 0;
+  data = 0;
+  
+  /************** VOLTAGE 1 MEASUREMENTS **************/
   ads1256.SetRegisterValue(MUX,B01000101); //AIN4 - AIN5
-  delayMicroseconds(5);
+  delay(50);
+  delayMicroseconds(10);
   ads1256.SendCMD(SYNC);
-  delayMicroseconds(5);
+  delayMicroseconds(10);
   ads1256.SendCMD(WAKEUP);
   
   data =  ads1256.GetConversion();
@@ -51,18 +57,91 @@ void loop() {
   if((data >= 0) && (data <= 8388607)){
     //positive
     f_data = ((float)data/8388607.0)*78.0;
-    f_data = (f_data-1.0678)/0.8237;
+    //f_data = (f_data-1.0678)/0.8237;
   }
   else if((data > 8388607) && (data <= 16777215)){
     //negative
-    /*bitClear(data, 23);
-    m_data = map(data, 0, 8388607, -255, -1);
-    m_data = map(m_data, -255, -1, -1, -78);*/
-    Serial.println("-");
+    bitClear(data, 23);
+    data = 8388607 - data;
+    f_data = ((float)data/8388607.0)*78.0;
+    f_data = -1*f_data;
+  }
+
+  if(visual){
+    Serial.print(f_data,4);
+    Serial.print(":");
+  }
+  else{
+    Serial.print("V1:");
+    Serial.print(f_data,4);
+    Serial.print(" | ");
   }
   
-  //Serial.print("Converted data: ");  
-  Serial.println(f_data,4);
+  /************** VOLTAGE 2 MEASUREMENTS **************/
+  ads1256.SetRegisterValue(MUX,B00100011); //AIN2 - AIN3
+  delay(50);
+  delayMicroseconds(10);
+  ads1256.SendCMD(SYNC);
+  delayMicroseconds(10);
+  ads1256.SendCMD(WAKEUP);
+  
+  data =  ads1256.GetConversion();
+  data = constrain(data, 0, 16777215);
+  
+  if((data >= 0) && (data <= 8388607)){
+    //positive
+    f_data = ((float)data/8388607.0)*78.0;
+    //f_data = (f_data-1.0678)/0.8237;
+  }
+  else if((data > 8388607) && (data <= 16777215)){
+    //negative
+    bitClear(data, 23);
+    data = 8388607 - data;
+    f_data = ((float)data/8388607.0)*78.0;
+    f_data = -1*f_data;
+  }
+  
+   if(visual){
+      Serial.print(f_data,4);
+      Serial.print(":");
+    }
+    else{
+      Serial.print("V2:");
+      Serial.print(f_data,4);
+      Serial.print(" | ");
+    }
+
+  /************** CURRENT MEASUREMENTS **************/
+  ads1256.SetRegisterValue(MUX,B00010000); //AIN0 - AIN1
+  delay(50);
+  delayMicroseconds(10);
+  ads1256.SendCMD(SYNC);
+  delayMicroseconds(10);
+  ads1256.SendCMD(WAKEUP);
+  
+  data =  ads1256.GetConversion();
+  data = constrain(data, 0, 16777215);
+  
+  if((data >= 0) && (data <= 8388607)){
+    //positive
+    f_data = ((float)data/8388607.0)*78.0;
+    //f_data = (f_data-1.0678)/0.8237;
+  }
+  else if((data > 8388607) && (data <= 16777215)){
+    //negative
+    bitClear(data, 23);
+    data = 8388607 - data;
+    f_data = ((float)data/8388607.0)*78.0;
+    f_data = -1*f_data;
+  }
+  if(visual){
+    Serial.println(f_data,4);
+  }
+  else{
+    Serial.print("I:");
+    Serial.println(f_data,4);
+  }
+  
   //delay(500);
 }
 
--- VI_DemoPCB/Processing_VIDemoPCB/Processing_VIDemoPCB.pde
+++ VI_DemoPCB/Processing_VIDemoPCB/Processing_VIDemoPCB.pde
@@ -4,28 +4,40 @@
 
 import processing.serial.*;
 Serial port;
-String dataRaw = "", dt ="";
+String dataRaw = "";
+String[] dt = {"", "", ""};
 PFont font;
 
 void setup()
 {
-  size(550,200);
+  size(550,350);
   port = new Serial(this, "COM4", 9600);
   port.bufferUntil('\n'); 
   font = loadFont("AgencyFB-Bold-200.vlw");
-  textFont(font, 100);
+  textFont(font, 80);
 }
 
 void draw()
 {
   background(0,0,0);
   fill(46, 209, 2);
-  text(dt, 50, 140);
-  text("mV", 400, 140);
+  text("V1:", 65, 100);
+  text(dt[0], 170, 100);
+  text("mV", 420, 100);
+  
+  text("V2:", 50, 200);
+  text(dt[1], 170, 200);
+  text("mV", 420, 200);
+  
+  fill(204, 0, 0);
+  text("V3:", 50, 300);
+  text(dt[2], 170, 300);
+  text("mV", 420, 300);
 }
   
 void serialEvent (Serial port)
 {
   dataRaw = port.readStringUntil('\n');
-  dt = dataRaw.substring(0, dataRaw.length() - 1);
+  dataRaw = dataRaw.substring(0, dataRaw.length()-1);
+  dt = split(dataRaw, ':');
 }
