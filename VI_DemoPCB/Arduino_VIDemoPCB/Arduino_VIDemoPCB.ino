11c00d5465be91d7db45728824a8988a27297a28
sideridispas <sideridispas@gmail.com>
2016-02-02 16:46:22 +0100

    Added elapsed time measuring

--- VI_DemoPCB/Arduino_VIDemoPCB/Arduino_VIDemoPCB.ino
+++ VI_DemoPCB/Arduino_VIDemoPCB/Arduino_VIDemoPCB.ino
@@ -33,9 +33,9 @@ DeviceAddress Probe04 = { 0x28, 0x39, 0xAF, 0xAF, 0x07, 0x00, 0x00, 0x70 };
 DeviceAddress Probe05 = { 0x28, 0xD8, 0xC0, 0xAE, 0x07, 0x00, 0x00, 0x9F };
 
 long data;
-float f_data;
+float f_data, V1, V2, I, P;
 ads12xx ads1256(7,2); //CS:7, DRDY:2
-int temp_res = 10; //temperature sensor resolution (9-12 bits)
+int temp_res = 11; //temperature sensor resolution (9-12 bits)
 
 int visual = 1; //processing app:1 - arduino serial monitor:0
 
@@ -48,7 +48,7 @@ void setup() {
   ads1256.SendCMD(SELFCAL); //self-calibration command
   delay(100); //wait the calibration
 
-  // set the resolution to 10 bit (Can be 9 to 12 bits .. lower is faster)
+  // set the resolution of temp sensors
   sensors.setResolution(Probe01, temp_res);
   sensors.setResolution(Probe02, temp_res);
   sensors.setResolution(Probe03, temp_res);
@@ -57,11 +57,12 @@ void setup() {
 }
 
 void loop() {
-
-  f_data = 0;
-  data = 0;
   
   /************** VOLTAGE 1 MEASUREMENTS **************/
+
+  //Get starting time
+  unsigned long StartTime = millis();
+
   ads1256.SetRegisterValue(MUX,B00100011); //AIN2 - AIN3
   delay(50);
   delayMicroseconds(10);
@@ -75,7 +76,7 @@ void loop() {
   if((data >= 0) && (data <= 8388607)){
     //positive
     f_data = ((float)data/8388607.0)*78.0;
-    f_data = (f_data+0.1386)/1.4742;
+    V1 = (f_data+0.1386)/1.4742;
   }
   else if((data > 8388607) && (data <= 16777215)){
     //negative
@@ -83,16 +84,16 @@ void loop() {
     data = 8388607 - data;
     f_data = ((float)data/8388607.0)*78.0;
     f_data = (f_data+0.1386)/1.4742;
-    f_data = -1*f_data;
+    V1 = -1*f_data;
   }
 
   if(visual){
-    Serial.print(f_data,4);
+    Serial.print(V1,4);
     Serial.print(":");
   }
   else{
     Serial.print("V1:");
-    Serial.print(f_data,4);
+    Serial.print(V1,4);
     Serial.print(" | ");
   }
   
@@ -110,7 +111,7 @@ void loop() {
   if((data >= 0) && (data <= 8388607)){
     //positive
     f_data = ((float)data/8388607.0)*78.0;
-    f_data = (f_data+0.1161)/1.4968;
+    V2 = (f_data+0.1161)/1.4968;
   }
   else if((data > 8388607) && (data <= 16777215)){
     //negative
@@ -118,16 +119,16 @@ void loop() {
     data = 8388607 - data;
     f_data = ((float)data/8388607.0)*78.0;
     f_data = (f_data+0.1161)/1.4968;
-    f_data = -1*f_data;
+    V2 = -1*f_data;
   }
   
    if(visual){
-      Serial.print(f_data,4);
+      Serial.print(V2,4);
       Serial.print(":");
     }
     else{
       Serial.print("V2:");
-      Serial.print(f_data,4);
+      Serial.print(V2,4);
       Serial.print(" | ");
     }
 
@@ -145,7 +146,7 @@ void loop() {
   if((data >= 0) && (data <= 8388607)){
     //positive
     f_data = ((float)data/8388607.0)*78.0;
-    f_data = (f_data-0.321)/4.9331;
+    I = (f_data-0.321)/4.9331;
   }
   else if((data > 8388607) && (data <= 16777215)){
     //negative
@@ -153,17 +154,29 @@ void loop() {
     data = 8388607 - data;
     f_data = ((float)data/8388607.0)*78.0;
     f_data = (f_data-0.321)/4.9331;
-    f_data = -1*f_data;
+    I = -1*f_data;
   }
   if(visual){
-    Serial.print(f_data,4);
+    Serial.print(I,4);
     Serial.print(":");
   }
   else{
     Serial.print("I:");
-    Serial.println(f_data,4);
+    Serial.print(I,4);
+    Serial.print(" | ");
   }
 
+  /************** TEMPERATURE CALCULATIONS **************/
+  P = V1 * I;
+  if(visual){
+    Serial.print(P,4);
+    Serial.print(":");
+  }
+  else{
+    Serial.print("P:");
+    Serial.println(P,4);
+  }
+  
   /************** TEMPERATURE MEASUREMENTS **************/
   // Command all devices on bus to read temperature  
   sensors.requestTemperatures();  
@@ -182,7 +195,7 @@ void loop() {
     Serial.print(" C:");
 
     printTemperature(Probe05);
-    Serial.println(" C");
+    Serial.print(" C:");
   }
   else{
     Serial.print("Probe 01 temperature is:   ");
@@ -205,10 +218,12 @@ void loop() {
     printTemperature(Probe05);
     Serial.println();
   }
-  
 
-  
-  //delay(500);
+  //Get end time
+  unsigned long CurrentTime = millis();
+  unsigned long ElapsedTime = CurrentTime - StartTime;
+  Serial.print(ElapsedTime);
+  Serial.println("ms");
 }
 
 /*-----( Declare User-written Functions )-----*/
--- VI_DemoPCB/Processing_VIDemoPCB/Processing_VIDemoPCB.pde
+++ VI_DemoPCB/Processing_VIDemoPCB/Processing_VIDemoPCB.pde
@@ -1,38 +1,65 @@
-//Program by Jeremy Blum
-//www.jeremyblum.com
-//Give you the temperature
-
 import processing.serial.*;
 Serial port;
 String dataRaw = "";
-String[] dt = {"", "", ""};
+String[] dt = {"", "", "", "", "", "","", "", "", ""};
 PFont font;
 
 void setup()
 {
-  size(550,350);
+  size(800, 450);
   port = new Serial(this, "COM4", 9600);
   port.bufferUntil('\n'); 
   font = loadFont("AgencyFB-Bold-200.vlw");
-  textFont(font, 80);
+  textFont(font, 60);
 }
 
 void draw()
 {
   background(0,0,0);
   fill(46, 209, 2);
-  text("V1:", 65, 100);
-  text(dt[0], 170, 100);
-  text("V", 420, 100);
+  textSize(60);
   
-  text("V2:", 50, 200);
-  text(dt[1], 170, 200);
-  text("V", 420, 200);
+  //voltages and current
+  text("V1:", 30, 70);
+  text(dt[0], 130, 70);
+  text("V", 320, 70);
+  
+  text("V2:", 30, 140);
+  text(dt[1], 130, 140);
+  text("V", 320, 140);
   
   fill(204, 0, 0);
-  text("I:", 100, 300);
-  text(dt[2], 170, 300);
-  text("A", 420, 300);
+  text("I:", 30, 210);
+  text(dt[2], 130, 210);
+  text("A", 320, 210);
+  
+  fill(255, 180, 20);
+  text("P:", 30, 280);
+  text(dt[3], 130, 280);
+  text("W", 320, 280);
+  
+  //temperatures
+  fill(255, 165, 0);
+  text("T1:", 490, 70);
+  text(dt[4], 560, 70);
+  
+  text("T2:", 490, 140);
+  text(dt[5], 560, 140);
+  
+  text("T3:", 490, 210);
+  text(dt[6], 560, 210);
+  
+  text("T4:", 490, 280);
+  text(dt[7], 560, 280);
+  
+  text("T5:", 490, 350);
+  text(dt[8], 560, 350);
+  
+  fill(200, 200, 200);
+  textSize(30);
+  text("Elapsed time:", 500, 420);
+  text(dt[9], 650, 420);
+
 }
   
 void serialEvent (Serial port)
