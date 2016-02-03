977bcbe1b9f0909bf7813949c7a45de7025c9316
sideridispas <sideridispas@gmail.com>
2016-02-03 17:20:55 +0100

    Added the RTC functions and object creation in main ino file. Also, created new interrupt (PIN3) that waits the 1Hz pulse from RTC. No timestamp added yet.

--- VI_DemoPCB/Arduino_VIDemoPCB/Arduino_VIDemoPCB.ino
+++ VI_DemoPCB/Arduino_VIDemoPCB/Arduino_VIDemoPCB.ino
@@ -9,6 +9,9 @@
 *                                                       *
 *    TEMP_1WIRE_BUS: PIN 3                              *
 *                                                       *
+*    RTC:   SQW | CLK | MISO |  MOSI |  SS              *                                      
+*           3     13    12      11      8               *
+*                                                       *
 ********************************************************/
 
 /*-----( Import needed libraries )-----*/
@@ -18,12 +21,16 @@
 
 /*-----( Declare Constants and Pin Numbers )-----*/
 #define ONE_WIRE_BUS_PIN 4
+#define ADC_CS 7
+#define ADC_DRDY 2
+#define RTC_CS 8
+#define RTC_SQW 3
 
 /*-----( Declare objects )-----*/
 // Setup a oneWire instance to communicate with any OneWire devices
 OneWire oneWire(ONE_WIRE_BUS_PIN);
 // Setup a ads12xx object to communicate with the ADS1256 ADC
-ads12xx ads1256(7,2); //CS:7, DRDY:2
+ads12xx ads1256(ADC_CS,ADC_DRDY);
 
 // Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
@@ -38,6 +45,8 @@ DeviceAddress Probe05 = { 0x28, 0xD8, 0xC0, 0xAE, 0x07, 0x00, 0x00, 0x9F };
 long data;
 float f_data, V1, V2, I, P;
 
+volatile int RTC_state = HIGH;
+
 int temp_res = 11; //temperature sensor resolution (9-12 bits)
 
 int visual = 1; //processing app:1 - arduino serial monitor:0
@@ -46,7 +55,7 @@ void setup() {
   // start serial port to show results
   Serial.begin(9600);
   ads1256.begin();
-  reg_init();  //register initialisation
+  reg_init();  //ADC's register initialisation
   sensors.begin(); //Initialize the Temperature measurement library
   ads1256.SendCMD(SELFCAL); //self-calibration command
   delay(100); //wait the calibration
@@ -57,6 +66,13 @@ void setup() {
   sensors.setResolution(Probe03, temp_res);
   sensors.setResolution(Probe04, temp_res);
   sensors.setResolution(Probe05, temp_res);
+
+  RTC_init();
+  //day(1-31), month(1-12), year(0-99), hour(0-23), minute(0-59), second(0-59)
+  SetTimeDate(3,2,16,15,17,02); 
+
+  //interrupt for waiting the 1Hz pulse of RTC
+  attachInterrupt(digitalPinToInterrupt(RTC_SQW), RTC_Interrupt, FALLING);
 }
 
 void loop() {
@@ -224,6 +240,11 @@ void loop() {
     Serial.println();
   }
 
+  waitforRTC();
+  noInterrupts();
+  RTC_state = HIGH;
+  interrupts();
+
   //Get end time
   unsigned long CurrentTime = millis();
   unsigned long ElapsedTime = CurrentTime - StartTime;
@@ -233,11 +254,9 @@ void loop() {
 
 /*-----( Declare User-written Functions )-----*/
 
-
 void reg_init(){
   unsigned long reg;
   
-  /***** REGISTER INITIALISATION ******/
   //STATUS register (default: 48)
   reg = ads1256.GetRegisterValue(STATUS);
   //reg = reg | B00000010; //BUFEN:1
@@ -274,4 +293,98 @@ float tempC = sensors.getTempC(deviceAddress);
    //Serial.print(" F: ");
    //Serial.print(DallasTemperature::toFahrenheit(tempC));
    }
+}
+
+int RTC_init(){ 
+    pinMode(RTC_CS,OUTPUT); // chip select
+    // start the SPI library:
+    SPI.begin();
+    SPI.setBitOrder(MSBFIRST); 
+    SPI.setDataMode(SPI_MODE1); // both mode 1 & 3 should work 
+    //set control register 
+    digitalWrite(RTC_CS, LOW);  
+    SPI.transfer(0x8E);
+    SPI.transfer(0x60); //60= disable Osciallator and Battery SQ wave @1hz, temp compensation, Alarms disabled
+    digitalWrite(RTC_CS, HIGH);
+    delay(10);
+}
+
+int SetTimeDate(int d, int mo, int y, int h, int mi, int s){ 
+  int TimeDate [7]={s,mi,h,0,d,mo,y};
+  for(int i=0; i<=6;i++){
+    if(i==3)
+      i++;
+    int b= TimeDate[i]/10;
+    int a= TimeDate[i]-b*10;
+    if(i==2){
+      if (b==2)
+        b=B00000010;
+      else if (b==1)
+        b=B00000001;
+    } 
+    TimeDate[i]= a+(b<<4);
+      
+    digitalWrite(RTC_CS, LOW);
+    SPI.transfer(i+0x80); 
+    SPI.transfer(TimeDate[i]);        
+    digitalWrite(RTC_CS, HIGH);
+  }
+}
+
+String ReadTimeDate(){
+  String temp;
+  int TimeDate [7]; //second,minute,hour,null,day,month,year    
+  for(int i=0; i<=6;i++){
+    if(i==3)
+      i++;
+    digitalWrite(RTC_CS, LOW);
+    SPI.transfer(i+0x00); 
+    unsigned int n = SPI.transfer(0x00);        
+    digitalWrite(RTC_CS, HIGH);
+    int a=n & B00001111;    
+    if(i==2){ 
+      int b=(n & B00110000)>>4; //24 hour mode
+      if(b==B00000010)
+        b=20;        
+      else if(b==B00000001)
+        b=10;
+      TimeDate[i]=a+b;
+    }
+    else if(i==4){
+      int b=(n & B00110000)>>4;
+      TimeDate[i]=a+b*10;
+    }
+    else if(i==5){
+      int b=(n & B00010000)>>4;
+      TimeDate[i]=a+b*10;
+    }
+    else if(i==6){
+      int b=(n & B11110000)>>4;
+      TimeDate[i]=a+b*10;
+    }
+    else{ 
+      int b=(n & B01110000)>>4;
+      TimeDate[i]=a+b*10; 
+      }
+  }
+  temp.concat(TimeDate[4]);
+  temp.concat("/") ;
+  temp.concat(TimeDate[5]);
+  temp.concat("/") ;
+  temp.concat(TimeDate[6]);
+  temp.concat("     ") ;
+  temp.concat(TimeDate[2]);
+  temp.concat(":") ;
+  temp.concat(TimeDate[1]);
+  temp.concat(":") ;
+  temp.concat(TimeDate[0]);
+  return(temp);
+}
+
+void RTC_Interrupt(){
+  RTC_state = LOW;  
+}
+
+void waitforRTC() {
+  while (RTC_state) continue;
 }
