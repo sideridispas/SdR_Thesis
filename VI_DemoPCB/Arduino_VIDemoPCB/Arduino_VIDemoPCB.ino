f0f80dd71346a35861849ec68795b7f69a3fc632
sideridispas <sideridispas@gmail.com>
2016-02-17 15:07:02 +0100

    Created new library for the RTC, named ds3234.h. Added functions RTC_Init, SetTimeDate, ReadTimeDate to library ds3234.h

--- VI_DemoPCB/Arduino_VIDemoPCB/Arduino_VIDemoPCB.ino
+++ VI_DemoPCB/Arduino_VIDemoPCB/Arduino_VIDemoPCB.ino
@@ -18,6 +18,7 @@
 #include "IC_Libs/ads12xx.h" //ADC ADS1256 libraries
 #include "IC_Libs/OneWire/OneWire.h" //Temperature sensor DS18B20 libraries
 #include "IC_Libs/dallas-temperature-control/DallasTemperature.h"
+#include "IC_Libs/ds3234_RTC/ds3234.h"
 #include <Wire.h>
 
 /*-----( Declare Constants and Pin Numbers )-----*/
@@ -34,7 +35,9 @@ OneWire oneWire(ONE_WIRE_BUS_PIN);
 // Setup a ads12xx object to communicate with the ADS1256 ADC
 ads12xx ads1256(ADC_CS,ADC_DRDY);
 // Pass our oneWire reference to Dallas Temperature.
-DallasTemperature sensors(&oneWire);
+DallasTemperature temp_sensors(&oneWire);
+// Setup a ds3234 instance
+ds3234 rtc(RTC_CS);
 
 /*-----( Declare Variables )-----*/
 // DS18B20 Temperature sensors addresses
@@ -57,21 +60,21 @@ void setup() {
   Serial.begin(9600); //start serial port to show results
   ads1256.begin(); //begin init for ADC chip
   ads1256.reg_init();  //ADC's register initialisation
-  sensors.begin(); //initialize the Temperature measurement library
+  temp_sensors.begin(); //initialize the Temperature measurement library
 
   delay(200); //wait for voltage reference to be stable
   ads1256.SendCMD(SELFCAL); //ADC self-calibration command
   
   // set the resolution of temp sensors
-  sensors.setResolution(Probe01, temp_res);
-  sensors.setResolution(Probe02, temp_res);
-  sensors.setResolution(Probe03, temp_res);
-  sensors.setResolution(Probe04, temp_res);
-  sensors.setResolution(Probe05, temp_res);
+  temp_sensors.setResolution(Probe01, temp_res);
+  temp_sensors.setResolution(Probe02, temp_res);
+  temp_sensors.setResolution(Probe03, temp_res);
+  temp_sensors.setResolution(Probe04, temp_res);
+  temp_sensors.setResolution(Probe05, temp_res);
 
-  RTC_init();//initialize of RTC chip
+  rtc.RTC_init();//initialize of RTC chip
   //Seting the timestamp [day(1-31), month(1-12), year(0-99), hour(0-23), minute(0-59), second(0-59)]
-  SetTimeDate(16,2,16,12,29,00); 
+  rtc.SetTimeDate(16,2,16,12,29,00); 
 
   //interrupt for waiting the 1Hz pulse of RTC
   attachInterrupt(digitalPinToInterrupt(RTC_SQW), RTC_Interrupt, FALLING);
@@ -110,13 +113,13 @@ void loop() {
   P = V1 * I;
   
   // TEMPERATURE MEASUREMENTS  
-  sensors.requestTemperatures(); //Command all devices on bus to read temperature  
+  temp_sensors.requestTemperatures(); //Command all devices on bus to read temperature  
 
-  tempC1 = sensors.printTemperature(Probe01);
-  tempC2 = sensors.printTemperature(Probe02);
-  tempC3 = sensors.printTemperature(Probe03);
-  tempC4 = sensors.printTemperature(Probe04);
-  tempC5 = sensors.printTemperature(Probe05);
+  tempC1 = temp_sensors.printTemperature(Probe01);
+  tempC2 = temp_sensors.printTemperature(Probe02);
+  tempC3 = temp_sensors.printTemperature(Probe03);
+  tempC4 = temp_sensors.printTemperature(Probe04);
+  tempC5 = temp_sensors.printTemperature(Probe05);
 
   
   // DATASTRING FILLING
@@ -141,7 +144,7 @@ void loop() {
   dataString3.concat(",");
   dataString3 = dataString3 + String(P,4);
   dataString3.concat(",");
-  dataString3.concat(ReadTimeDate());
+  dataString3.concat(rtc.ReadTimeDate());
   dataString3.concat(",");
 
   Serial.print(dataString1);
@@ -169,96 +172,6 @@ void loop() {
 }
 
 /*-----( Declare User-written Functions )-----*/
-
-
-
-
-
-int RTC_init(){ 
-    pinMode(RTC_CS,OUTPUT); // chip select
-    // start the SPI library:
-    SPI.begin();
-    SPI.setBitOrder(MSBFIRST); 
-    SPI.setDataMode(SPI_MODE1); // both mode 1 & 3 should work 
-    //set control register 
-    digitalWrite(RTC_CS, LOW);  
-    SPI.transfer(0x8E);
-    SPI.transfer(B01100000); //60= disable Osciallator and Battery SQ wave @1hz, temp compensation, Alarms disabled
-    digitalWrite(RTC_CS, HIGH);
-    delay(10);
-}
-
-int SetTimeDate(int d, int mo, int y, int h, int mi, int s){ 
-  int TimeDate [7]={s,mi,h,0,d,mo,y};
-  for(int i=0; i<=6;i++){
-    if(i==3)
-      i++;
-    int b= TimeDate[i]/10;
-    int a= TimeDate[i]-b*10;
-    if(i==2){
-      if (b==2)
-        b=B00000010;
-      else if (b==1)
-        b=B00000001;
-    } 
-    TimeDate[i]= a+(b<<4);
-      
-    digitalWrite(RTC_CS, LOW);
-    SPI.transfer(i+0x80); 
-    SPI.transfer(TimeDate[i]);        
-    digitalWrite(RTC_CS, HIGH);
-  }
-}
-
-String ReadTimeDate(){
-  String temp;
-  int TimeDate [7]; //second,minute,hour,null,day,month,year    
-  for(int i=0; i<=6;i++){
-    if(i==3)
-      i++;
-    digitalWrite(RTC_CS, LOW);
-    SPI.transfer(i+0x00); 
-    unsigned int n = SPI.transfer(0x00);        
-    digitalWrite(RTC_CS, HIGH);
-    int a=n & B00001111;    
-    if(i==2){ 
-      int b=(n & B00110000)>>4; //24 hour mode
-      if(b==B00000010)
-        b=20;        
-      else if(b==B00000001)
-        b=10;
-      TimeDate[i]=a+b;
-    }
-    else if(i==4){
-      int b=(n & B00110000)>>4;
-      TimeDate[i]=a+b*10;
-    }
-    else if(i==5){
-      int b=(n & B00010000)>>4;
-      TimeDate[i]=a+b*10;
-    }
-    else if(i==6){
-      int b=(n & B11110000)>>4;
-      TimeDate[i]=a+b*10;
-    }
-    else{ 
-      int b=(n & B01110000)>>4;
-      TimeDate[i]=a+b*10; 
-      }
-  }
-  temp.concat(TimeDate[4]);
-  temp.concat("/") ;
-  temp.concat(TimeDate[5]);
-  temp.concat("/") ;
-  temp.concat(TimeDate[6]);
-  temp.concat(" ") ;
-  temp.concat(TimeDate[2]);
-  temp.concat(":") ;
-  temp.concat(TimeDate[1]);
-  temp.concat(":") ;
-  temp.concat(TimeDate[0]);
-  return(temp);
-}
 
 void RTC_Interrupt(){
   RTC_state = LOW;  
---
+++ VI_DemoPCB/Arduino_VIDemoPCB/IC_Libs/ds3234_RTC/ds3234.cpp
@@ -0,0 +1,91 @@
+#include "ds3234.h"
+
+ds3234::ds3234(const int RTC_CS) {
+    _RTC_CS = RTC_CS;
+}
+
+int ds3234::RTC_init(){ 
+    pinMode(_RTC_CS,OUTPUT); // chip select
+    // start the SPI library:
+    SPI.begin();
+    SPI.setBitOrder(MSBFIRST); 
+    SPI.setDataMode(SPI_MODE1); // both mode 1 & 3 should work 
+    //set control register 
+    digitalWrite(_RTC_CS, LOW);  
+    SPI.transfer(0x8E);
+    SPI.transfer(B01100000); //60= disable Osciallator and Battery SQ wave @1hz, temp compensation, Alarms disabled
+    digitalWrite(_RTC_CS, HIGH);
+    delay(10);
+}
+
+int ds3234::SetTimeDate(int d, int mo, int y, int h, int mi, int s){ 
+  int TimeDate [7]={s,mi,h,0,d,mo,y};
+  for(int i=0; i<=6;i++){
+    if(i==3)
+      i++;
+    int b= TimeDate[i]/10;
+    int a= TimeDate[i]-b*10;
+    if(i==2){
+      if (b==2)
+        b=B00000010;
+      else if (b==1)
+        b=B00000001;
+    } 
+    TimeDate[i]= a+(b<<4);
+      
+    digitalWrite(_RTC_CS, LOW);
+    SPI.transfer(i+0x80); 
+    SPI.transfer(TimeDate[i]);        
+    digitalWrite(_RTC_CS, HIGH);
+  }
+}
+
+String ds3234::ReadTimeDate(){
+  String temp;
+  int TimeDate [7]; //second,minute,hour,null,day,month,year    
+  for(int i=0; i<=6;i++){
+    if(i==3)
+      i++;
+    digitalWrite(_RTC_CS, LOW);
+    SPI.transfer(i+0x00); 
+    unsigned int n = SPI.transfer(0x00);        
+    digitalWrite(_RTC_CS, HIGH);
+    int a=n & B00001111;    
+    if(i==2){ 
+      int b=(n & B00110000)>>4; //24 hour mode
+      if(b==B00000010)
+        b=20;        
+      else if(b==B00000001)
+        b=10;
+      TimeDate[i]=a+b;
+    }
+    else if(i==4){
+      int b=(n & B00110000)>>4;
+      TimeDate[i]=a+b*10;
+    }
+    else if(i==5){
+      int b=(n & B00010000)>>4;
+      TimeDate[i]=a+b*10;
+    }
+    else if(i==6){
+      int b=(n & B11110000)>>4;
+      TimeDate[i]=a+b*10;
+    }
+    else{ 
+      int b=(n & B01110000)>>4;
+      TimeDate[i]=a+b*10; 
+      }
+  }
+  temp.concat(TimeDate[4]);
+  temp.concat("/") ;
+  temp.concat(TimeDate[5]);
+  temp.concat("/") ;
+  temp.concat(TimeDate[6]);
+  temp.concat(" ") ;
+  temp.concat(TimeDate[2]);
+  temp.concat(":") ;
+  temp.concat(TimeDate[1]);
+  temp.concat(":") ;
+  temp.concat(TimeDate[0]);
+  return(temp);
+}
