82075ba973c0a7bc7ba1dbd5d91d3db20e8f94b9
sideridispas <sideridispas@gmail.com>
2016-05-26 15:41:56 +0200

    Calibration is COMPLETE! 2 boxes are ready.

--- Calibration/Archive/Paschalis.m
+++ Calibration/Archive/Paschalis.m
@@ -3,27 +3,17 @@ clear all
 clc
 
 %Load data
-% PCB = load('./After/PCB_data.csv');
-% DMM = load('./After/DMM_data.csv');
+PCB = load('./before/PCB_data_before.csv');
+DMM = load('./before/DMM_data_before.csv');
 
-load('./BOX1/I/After/data.mat')
+DMM = [DMM; NaN(size(PCB,1)-size(DMM,1),1)];
 
-%DMM = [DMM; NaN(size(PCB,1)-size(DMM,1),1)];
-
-DMM = DMM(~isnan(DMM));
-PCB = PCB(~isnan(PCB));
-
-% plot both sets
-figure
-hold all
-plot(PCB)
-plot(DMM)
-legend('PCB','DMM','location','south')
-saveas(gcf, 'RawData', 'png')
+%filter the peak values
+PCB = medfilt1(PCB);
 
 % find the steps for PCB
-a = find(diff(PCB)> 0.1);
-b = find(diff(PCB)< -0.1);
+a = find(diff(PCB)> 0.2);
+b = find(diff(PCB)< -0.2);
 intervals_PCB = [a; b];
 intervals_PCB = sort(intervals_PCB);
 % make sure only the steps are registered and not a small deviation
@@ -31,13 +21,19 @@ a = find(diff(intervals_PCB)>15);
 intervals_PCB = intervals_PCB(a);
 
 % find the steps for DMM
-a = find(diff(DMM)> 0.1);
-b = find(diff(DMM)< -0.1);
+a = find(diff(DMM)> 0.2);
+b = find(diff(DMM)< -0.2);
 intervals_DMM = [a; b];
 intervals_DMM = sort(intervals_DMM);
 % make sure only the steps are registered and not a small deviation
-a = find(diff(intervals_DMM)>13);
+a = find(diff(intervals_DMM)>12);
 intervals_DMM = intervals_DMM(a);
+
+% plot both sets
+figure
+plot([PCB DMM]);
+legend('PCB','DMM')
+saveas(gcf, 'RawData', 'png')
 
 disp(['intervals PCB: ' int2str(size(intervals_PCB,1)) ', intervals DMM: ' int2str(size(intervals_DMM,1))])
 
@@ -46,27 +42,27 @@ for i = 1:size(intervals_PCB,1)-1
     % prin the interval
     %disp(['interval: ' int2str(i) ' - ' int2str(intervals_PCB(i+1)-intervals_PCB(i)) ' elementen '])
     % select the values: remove the two first and last measurements
-    list = [PCB(intervals_PCB(i)+2:intervals_PCB(i+1)-2)]';   
+    list = [PCB(intervals_PCB(i)+2:intervals_PCB(i+1)-2)]';      
     % clean up the NaN
     list = list(~isnan(list));
     % calc the mean
     PCBmean(i) = mean(list);
     %disp(['mean: ' int2str(PCBmean(i))])
 end
+PCBmean = PCBmean(~isnan(PCBmean));
 PCBmean = PCBmean';
-
-%disp(['hello========'])
 
 % for every interval between two steps do
 for i = 1:size(intervals_DMM,1)-1
     % prin the interval
     %disp(['interval: ' int2str(i) ' - ' int2str(intervals_DMM(i+1)-intervals_DMM(i)) ' elementen '])
     % select the values: remove the two first and last measurements
-    list = [DMM(intervals_DMM(i)+2:intervals_DMM(i+1)-2)]';     
+    list = [DMM(intervals_DMM(i)+2:intervals_DMM(i+1)-2)]';      
     % calc the mean
     DMMmean(i) = mean(list);
     %disp(['mean: ' int2str(DMMmean(i))])
 end
+DMMmean = DMMmean(~isnan(DMMmean));
 DMMmean = DMMmean';
 
 % plot
@@ -86,18 +82,14 @@ ylabel('difference')
 xlabel('DMM reading')
 saveas(gcf, 'difference', 'png')
 
-%save
-data = [DMMmean PCBmean];
-xlswrite('toCalibrate.xlsx',data)
-
 % plot to do calibration
-figure 
-scatter(DMMmean(1:end-2),PCBmean(1:end-2))
-ylabel('PCB')
-xlabel('DMM reading')
+% figure 
+% scatter(DMMmean(1:end-2),PCBmean(1:end-2))
+% ylabel('PCB')
+% xlabel('DMM reading')
 p = polyfit(DMMmean(1:end-2),PCBmean(1:end-2),1);
 disp(['y = ' sprintf('%0.5f',p(1)) ' x + ' sprintf('%0.5f',p(2))])
-saveas(gcf, 'scatter', 'png')
+%saveas(gcf, 'scatter', 'png')
 
 D = abs(DMMmean-PCBmean).^2;
 MSE = sum(D(:))/numel(DMMmean)
